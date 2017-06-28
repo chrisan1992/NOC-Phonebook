a52063a135c749628d6d62e73b17b532286d5319
Christopher Sanchez <chrisan1992@gmail.com>
2017-06-27 18:23:32 -0600

    ALM QC and PC tabs

---
+++ NOC Phonebook/Phonebook Tabs/alm_pc_user_control.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace NOC_Phonebook.Phonebook_Tabs
+{
+    partial class alm_pc_user_control
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // alm_pc_user_control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "alm_pc_user_control";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Size = new System.Drawing.Size(743, 395);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ NOC Phonebook/Phonebook Tabs/alm_qc_user_control.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NOC_Phonebook.Phonebook_Tabs
+{
+    public partial class alm_qc_user_control : UserControl
+    {
+        private static LyncCall call;
+
+        public alm_qc_user_control()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+
+        }
+
+        // Declare a Name property of type string:
+        public LyncCall Call
+        {
+            get
+            {
+                return call;
+            }
+            set
+            {
+                call = value;
+            }
+        }
+    }
+}
